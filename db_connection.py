e04bdae57379e572d3e908c097875abd6cf3572e
hannah <hgsherid@uvm.edu>
2024-01-30 17:01:59 -0500

    changed cred path

--- db_connection.py
+++ db_connection.py
@@ -1,10 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import firestorecd
 
 
 def db_connection():
-    cred_path = "../pythonProject6/warmup-project-61aa4-firebase-adminsdk-4d13y-72a1118150.json"
+    cred_path = "../warm-up-project-3050/warmup-project-61aa4-firebase-adminsdk-4d13y-72a1118150.json"
     cred = credentials.Certificate(cred_path)
     firebase_admin.initialize_app(cred)
     db = firestore.client()
