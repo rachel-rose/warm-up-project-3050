baddaf4590360f9aaa52d15fd532577c22a97a73
hannah <hgsherid@uvm.edu>
2024-01-30 18:33:22 -0500

    load json data to firestore

---
+++ admin.py
@@ -0,0 +1,21 @@
+import json
+from db_connection import db_connection
+
+
+def load_data():
+    jsondata = "../warm-up-project-3050/movies-data.json"
+    uuid = 0
+
+    # Connect to database
+    db = db_connection()
+
+    # Create Movies collection
+    movies_ref = db.collection("Movies")
+
+    # Load json data to firestore db
+    with open(jsondata, 'r') as inFile:
+        data = json.load(inFile)
+
+    for movie in data:
+        uuid += 1
+        movies_ref.document(str(uuid)).set(movie)
--- db_connection.py
+++ db_connection.py
@@ -2,9 +2,6 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-import requests
-import json
-
 cred_path = "../warm-up-project-3050/warmup-project-61aa4-firebase-adminsdk-4d13y-72a1118150.json"
 
 
@@ -14,31 +11,3 @@ def db_connection():
     db = firestore.client()
 
     return db
-
-
-url = "https://warmup-project-61aa4.firebaseio.com/movies-data.json"
-jsondata = "../warm-up-project-3050/movies-data.json"
-db = db_connection()
-movies_ref = db.collection("Movies").document()
-
-with open(jsondata, 'r') as inFile:
-    data = json.load(inFile)
-
-for movie in data:
-    movies_ref.set(movie)
-
-
-
-
-"""
-class Movie:
-    def __init__(self, name, rating):
-        self.name = name
-        self.rating = rating
-"""
-
-
-#movies_ref.document("Whiplash").set(Movie("Whiplash", 4.5).to_dict())
-
-
-#movies_ref.document("G-Force").set(Movie("G-Force", 1.2).to_dict())
