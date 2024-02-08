7820180db13e6433f1cf4f1c39c5f3562ae491aa
Sebas-Thomas <sothomas@uvm.edu>
2024-02-08 17:13:44 -0500

    Started Command Line

--- admin.py
+++ admin.py
@@ -1,8 +1,17 @@
+import sys
 from queries import Work
 
 if __name__ == "__main__":
-    filename = "warm-up-project-3050/movies-data.json" # User will give this as command line arg?
+    while True:
+        if len(sys.argv) < 2:
+            print("ERROR! PLEASE ENTER JUST THE DATABASE NAME")
+        else:
+            filename = sys.argv[1]
+            print(filename)
 
-    work = Work()
-    work.clear_data()
-    work.load_data(filename)
+
+    # filename = "warm-up-project-3050/movies-data.json" # User will give this as command line arg?
+    #
+    # work = Work()
+    # work.clear_data()
+    # work.load_data(filename)
